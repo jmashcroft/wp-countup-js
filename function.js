27add1440049f7a17f083749181a8c22a499152d
jmashcroft <james@greatdigital.co.uk>
2018-11-03 08:23:51 +0000

    Create function.js

---
+++ function.js
@@ -0,0 +1,22 @@
+ $(".st-animate").click(function(e) {
+      e.preventDefault();
+      $(this).closest('li').find('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
+        {
+          duration: 4000,
+          easing:'linear',
+          decimal: '.',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+        });
+      });
+     });
